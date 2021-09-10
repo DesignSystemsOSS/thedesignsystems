68dadbafa73bebbb37a7211edde1e7659ac1f18f
yashsehgal <yashsehgal.work@gmail.com>
2021-09-11 04:55:29 +0530

    feat: landing-page section added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/section-components/Header";
+import './styles/index.css';
+import LandingPage from "./views/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center__view App">
+      {/* header component */}
+      <Header />
+      <BrowserRouter>
+        <Route path="/" exact component={LandingPage} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/LandingPage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../../components/widgets/Button';
+import './style.view.landing-page.css';
+
+export default function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="updates-alert-toast">
+        <span className="new-tag">new</span>
+        <p className="alert-title">We are revamping eccentric-touch css library</p>
+      </div>
+      <p className="massive-tagline">
+        We are building <span className="gradient-text">frontend</span> solutions for developers
+      </p>
+      <p className="landing-page-description">
+        The DesignSystems' tools helps you write more scalable code without worrying about design mistakes. Built for developers and teams to save their time.
+      </p>
+      <div className="cta-buttons-wrapper button-wrapper button-wrapper-center">
+        <Button value="Get started" type="primary" />
+        <Button value="Check our GitHub" type="outline" startIcon="fab fa-github" />
+      </div>
+    </div>
+  )
+}
