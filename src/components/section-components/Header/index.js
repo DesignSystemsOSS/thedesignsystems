e49dc930b8e554ed22cf737bca4b6e1c737784cf
yashsehgal <yashsehgal.work@gmail.com>
2021-09-11 19:33:28 +0530

    update: header logo link added

--- src/components/section-components/Header/index.js
+++ src/components/section-components/Header/index.js
@@ -7,10 +7,12 @@ export default function Header() {
   const [headerOptions] = useState(HeaderOptions);
   return (
     <header className="header">
-      <div className="logo-wrapper">
-        <h1 className="demo-logo-icon">DS</h1>
-        <p className="logo-title">The DesignSystems</p>
-      </div>
+      <a href="/" className="logo-wrapper-link">
+        <div className="logo-wrapper">
+          <h1 className="demo-logo-icon">DS</h1>
+          <p className="logo-title">The DesignSystems</p>
+        </div>
+      </a>
       <div className="header-options-wrapper">
         <ul className="header-options-list">
           {headerOptions.map((data, index) => {
