dbf147f3c728a9ac808c39572300f3cac33234c0
yashsehgal <yashsehgal.work@gmail.com>
2021-09-11 19:32:53 +0530

    update: routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route } from "react-router-dom";
+// import Footer from "./components/section-components/Footer";
 import Header from "./components/section-components/Header";
 import './styles/index.css';
+import Careers from "./views/Careers";
 import LandingPage from "./views/LandingPage";
+import Products from "./views/Products";
 import Team from "./views/Team";
 
 function App() {
@@ -12,7 +15,11 @@ function App() {
       <BrowserRouter>
         <Route path="/" exact component={LandingPage} />
         <Route path="/team" exact component={Team} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/careers" exact component={Careers} />
       </BrowserRouter>
+      {/* footer component */}
+      {/* <Footer /> */}
     </div>
   );
 }
