3ec4b1c7cd5a5a45e43021c265b0744c64b1f9d9
yashsehgal <yashsehgal.work@gmail.com>
2021-09-19 15:03:08 +0530

    feat: personal-links feature added to core workers and maintainers :rocket:

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Footer from "./components/section-components/Footer";
 import Header from "./components/section-components/Header";
 import './styles/index.css';
 import Careers from "./views/Careers";
-import Discord from "./views/Discord";
+import Discord from "./views/Redirect/Discord";
 import LandingPage from "./views/LandingPage";
 import Products from "./views/Products";
 import Team from "./views/Team";
+import Twitter from "./views/Redirect/Twitter";
+import GitHub from "./views/Redirect/GitHub";
+import LinkedIn from "./views/Redirect/Linkedin";
+import PersonalLinks from "./views/PersonalLinks";
 
 function App() {
   return (
@@ -19,8 +23,12 @@ function App() {
         <Route path="/team" exact component={Team} />
         <Route path="/products" exact component={Products} />
         <Route path="/careers" exact component={Careers} />
-        {/* route for re-directing to the discord server */}
+        {/* routes for re-direction to external links */}
         <Route path="/discord" exact component={Discord} />
+        <Route path="/twitter" exact component={Twitter} />
+        <Route path="/github" exact component={GitHub} />
+        <Route path="/linkedin" exact component={LinkedIn} />
+        <Route path="/yash" exact component={() => PersonalLinks('yash')} />
       </BrowserRouter>
       {/* footer component */}
       <Footer />
