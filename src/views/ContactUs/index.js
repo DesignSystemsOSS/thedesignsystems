6e6cccde61ec3712bb8647c4e88ec7b010312209
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-18 14:00:21 +0000

    feat: contact-us forrm added

--- src/views/ContactUs/index.js
+++ src/views/ContactUs/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.view.contact-us.css';
 import Button from '../../components/widgets/Button';
-import img from '../../assets/Illustration.png';
+import ContactUsForm from './ContactUsForm';
 
 export default function ContactUs() {
   return (
@@ -11,38 +11,9 @@ export default function ContactUs() {
         Be in touch with us, and collaborate with the community members and grow
         in a team.
       </p>
-
-      <div className="form-section-wrapper">
-        <img src={img} alt="Contact-Us-Illustration" className="contact-illustration" />
-        
-        <form className="contact-us-form" action="" method="">
-          <label for="name">
-            Name
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="name-input"
-              id="name"
-              name="name"
-            />
-          </label>
-          <label for="email">
-            Email Id
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="email-input"
-              id="email-id"
-              name="email-id"
-            />
-          </label>
-          <label for="message">Message</label>
-          <textarea name="textarea" id="textarea-form-wrapper" cols="30" rows="5">
-          </textarea>
-          <a href="/" className="submit-form-button">
-            <Button type="primary" value="Submit" className="submit-btn"/>
-          </a>
-        </form>
+      {/* form component */}
+      <div className="contact-us-form-wrapper">
+        <ContactUsForm />
       </div>
     </div>
   );
